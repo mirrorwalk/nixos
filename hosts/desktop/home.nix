8d8c79e266f8fa8f3a8de221950da2bf823dbcb1
mirrorwalk <git.cresting327@passmail.net>
2025-07-20 17:42:25 +0200

    Backup: 2025-07-20 17:42:25

--- hosts/desktop/home.nix
+++ hosts/desktop/home.nix
@@ -80,7 +80,7 @@
         }
             
         setopt PROMPT_SUBST
-        PS1="$ "
+        PS1=$'\e[36m[\e[33m%n\e[32m@\e[34m%m \e[35m%~\e[36m]\e[31m$(git_branch)\e[0m$ '
 
         export LC_CTYPE=en_US.UTF-8
         unset LC_ALL
