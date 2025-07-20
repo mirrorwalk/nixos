8e274d71d96ed27da8647bf77bf2c91521877999
mirrorwalk <git.cresting327@passmail.net>
2025-07-20 16:57:17 +0200

    Backup: 2025-07-20 16:57:17

--- hosts/desktop/home.nix
+++ hosts/desktop/home.nix
@@ -20,7 +20,6 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = [
-    pkgs.kitty
     pkgs.ghostty
     pkgs.tmux
     pkgs.librewolf
