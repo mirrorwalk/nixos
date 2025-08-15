b9279f37a3c04e25c49e8b93476b44f15d7cc4c7
mirrorwalk <git.cresting327@passmail.net>
2025-08-15 19:42:42 +0200

    Backup: 2025-08-15 19:42:42

--- hosts/desktop/home.nix
+++ hosts/desktop/home.nix
@@ -67,15 +67,15 @@
     pkgs.inotify-tools
   ];
 
-  gtk = {
-      enable = true;
-      theme = {
-          name = "Adwaita-dark";
-          package = pkgs.gnome-themes-extra;
-      };
-      gtk3.extraConfig.gtk-application-prefer-dark-theme = 1;
-      gtk4.extraConfig.gtk-application-prefer-dark-theme = 1;
-  };
+  # gtk = {
+  #     enable = true;
+  #     theme = {
+  #         name = "Adwaita-dark";
+  #         package = pkgs.gnome-themes-extra;
+  #     };
+  #     gtk3.extraConfig.gtk-application-prefer-dark-theme = 1;
+  #     gtk4.extraConfig.gtk-application-prefer-dark-theme = 1;
+  # };
 
   qt = {
       enable = true;
