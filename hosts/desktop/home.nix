4bdbb720109eb9c10b54e4dd33adf148e7db9f82
mirrorwalk <git.cresting327@passmail.net>
2025-08-26 18:14:11 +0200

    Backup: 2025-08-26 18:14:11

--- hosts/desktop/home.nix
+++ hosts/desktop/home.nix
@@ -158,6 +158,7 @@ dconf.settings = {
         enix = "cd $HOME/.config/nixos && nvim $HOME/.config/nixos";
         backup-system="$HOME/.local/bin/system-backup-git.sh";
         la= "ls -aF --color=auto";
+        tmuxs = "tmux new -s";
     };
 
     initContent = ''
