9c9b9c5a9f9e4b26c0b2d1c1af0cc1712c92e6ae
mirrorwalk <git.cresting327@passmail.net>
2025-08-29 23:48:57 +0200

    Backup: 2025-08-29 23:48:57

--- hosts/desktop/home.nix
+++ hosts/desktop/home.nix
@@ -265,8 +265,8 @@ dconf.settings = {
 # Other
 
         setopt PROMPT_SUBST
-        PS1='%F{cyan}[%F{yellow}%n%F{green}@%F{blue}%m %F{magenta}%~%F{cyan}]%F{red}$(git branch --show-current 2>/dev/null | sed "s/.*/(&)/")%f$ '
-            
+        PS1='%F{cyan}[%F{yellow}%n%F{green}@%F{blue}%m %F{magenta}%~%F{cyan}]%F{red}$(git branch --show-current 2>/dev/null | sed "s/.*/(&)/")%F{cyan}(%j)%f$ '            
+
         eval "$(direnv hook zsh)"
 
         autoload -U compinit
