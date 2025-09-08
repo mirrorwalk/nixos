4f11bea566f7a2d7789ba177305f69d0ac733fff
mirrorwalk <git.cresting327@passmail.net>
2025-09-08 18:01:42 +0200

    Backup: 2025-09-08 18:01:42

--- modules/home-manager/ssh/ssh.nix
+++ modules/home-manager/ssh/ssh.nix
@@ -1,13 +1,30 @@
 {
-    programs.ssh = {
-        enable = true;
-        enableDefaultConfig = false;
-        matchBlocks."*" = {
-            addKeysToAgent = "yes";
-            serverAliveInterval = 60;
-            serverAliveCountMax = 5;
-            forwardAgent = false;
-            compression = true;
-        };
+  programs.ssh = {
+    enable = true;
+    enableDefaultConfig = false;
+    matchBlocks."*" = {
+      identityFile = "~/.ssh/id_ed25519";
+
+      compression = true;
+
+      serverAliveInterval = 60;
+      serverAliveCountMax = 3;
+
+      extraOptions = {
+        AddKeysToAgent = "yes";
+
+        HashKnownHosts = "yes";
+
+        StrictHostKeyChecking = "accept-new";
+
+        ControlMaster = "auto";
+        ControlPath = "~/.ssh/master-%r@%n:%p";
+        ControlPersist = "15m";
+
+        Ciphers = "chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes128-gcm@openssh.com";
+        KexAlgorithms = "curve25519-sha256,curve25519-sha256@libssh.org";
+        MACs = "hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com";
+      };
     };
+  };
 }
