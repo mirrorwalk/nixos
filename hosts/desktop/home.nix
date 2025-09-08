e3d3d6c268c97669e9ff25cf9188c3f5ebfd3a14
mirrorwalk <git.cresting327@passmail.net>
2025-09-08 18:14:40 +0200

    Backup: 2025-09-08 18:14:40

--- hosts/desktop/home.nix
+++ hosts/desktop/home.nix
@@ -212,12 +212,13 @@
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
     # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
+    #
+    # # # You can also set the file content immediately.
     # ".gradle/gradle.properties".text = ''
     #   org.gradle.console=verbose
     #   org.gradle.daemon.idletimeout=3600000
     # '';
+    ".config/niri/config.kdl".source = ./dotfiles/niri-config;
   };
 
   # Home Manager can also manage your environment variables through
