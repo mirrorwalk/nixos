0ed15509aa0a1f00954599a27424decfb7632729
mirrorwalk <git.cresting327@passmail.net>
2025-10-04 01:46:15 +0200

    Backup: 2025-10-04 01:46:15

--- flake.nix
+++ flake.nix
@@ -8,11 +8,6 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    zen-browser = {
-      url = "github:0xc000022070/zen-browser-flake/beta";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     nix-index-database.url = "github:nix-community/nix-index-database";
     nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
   };
@@ -28,7 +23,6 @@
         specialArgs = {inherit inputs;};
         modules = [
           ./hosts/desktop/configuration.nix
-          inputs.home-manager.nixosModules.default
           nix-index-database.nixosModules.nix-index
         ];
       };
