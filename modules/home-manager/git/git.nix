578c82c0e8964c02ac5e159f17d89c49575d5e51
mirrorwalk <git.cresting327@passmail.net>
2025-10-22 16:39:59 +0200

    Backup: 2025-10-22 16:39:59

--- modules/home-manager/git/git.nix
+++ modules/home-manager/git/git.nix
@@ -77,18 +77,16 @@
       };
       user.signingkey = "~/.ssh/git";
     };
-
   };
 
   programs.delta = {
     enable = true;
     enableGitIntegration = true;
-      options = {
-        navigate = true;
-        light = false;
-        line-numbers = true;
-      };
-
+    options = {
+      navigate = true;
+      light = false;
+      line-numbers = true;
+    };
   };
 
   programs.ssh.matchBlocks = {
