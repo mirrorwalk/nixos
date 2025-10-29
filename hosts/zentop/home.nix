94b9d89f6fc6e78bfc6ecf6afd6ac0342ad67b72
mirrorwalk <git.cresting327@passmail.net>
2025-10-30 00:33:59 +0100

    Backup: 1761780838

--- hosts/zentop/home.nix
+++ hosts/zentop/home.nix
@@ -52,7 +52,6 @@
     pkgs.jq
     pkgs.kdePackages.dolphin
     pkgs.nix-output-monitor
-    pkgs.yt-dlp
   ];
 
   systemConfig.monitors = [
@@ -63,6 +62,27 @@
       refreshRate = 60.0;
     }
   ];
+
+  home.shellAliases = {
+    backup-message = "echo Backup: $(date '+%Y-%m-%d %H:%M:%S')";
+    evi = "cd $HOME/.config/nvim && nvim .";
+    la = "ls -AF --color=auto";
+    tmuxs = "${pkgs.tmux}/bin/tmux new -s";
+    tmuxa = "${pkgs.tmux}/bin/tmux attach-session -t nixos || ${pkgs.tmux}/bin/tmux switch-client -t ";
+    nix-shell = "${pkgs.nix-output-monitor}/bin/nom-shell";
+    nix-build = "${pkgs.nix-output-monitor}/bin/nom-build";
+    nix = "${pkgs.nix-output-monitor}/bin/nom";
+    cat = "${pkgs.bat}/bin/bat";
+    rnos = "${pkgs.nh}/bin/nh os switch";
+    nosr = "${pkgs.nh}/bin/nh os switch";
+    nr = "${pkgs.nh}/bin/nh os switch";
+    nt = "${pkgs.nh}/bin/nh os test";
+    rn = "${pkgs.nh}/bin/nh os switch";
+    ns = "${pkgs.nh}/bin/nh os switch";
+    nos = "${pkgs.nh}/bin/nh os switch";
+    nd = "${pkgs.nix-output-monitor}/bin/nom develop";
+    flake = "${pkgs.nix}/bin/nix flake";
+  };
 
   shells = {
     bash.enable = true;
