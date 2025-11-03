de2edb81d13428c97408b7fc98b7301a2c3f7443
mirrorwalk <git.cresting327@passmail.net>
2025-11-03 21:24:22 +0100

    Backup: 1762201461

--- modules/home-manager/hypr/hyprpaper.nix
+++ modules/home-manager/hypr/hyprpaper.nix
@@ -18,12 +18,38 @@
       )
     }
   '';
+
+  hyprpaperWrapper = inputs.wrappers.lib.wrapPackage {
+    inherit pkgs;
+    package = pkgs.hyprpaper;
+
+    env = {
+      XDG_CONFIG_HOME = hyprpaperConfig;
+    };
+  };
 in {
   config = lib.mkIf config.services.hyprpaper.enable {
+    services.hyprpaper = {
+        package = hyprpaperWrapper;
+    #   settings = {
+    #     preload = toString config.styleConfig.defaultWallpaper;
+    #
+    #     wallpaper =
+    #       map (
+    #         m: "${m.name},${toString config.styleConfig.defaultWallpaper}"
+    #       )
+    #       (
+    #         lib.filter
+    #         (m: m.enabled)
+    #         config.systemConfig.monitors
+    #       );
+    #   };
+    };
+
     systemd.user.services.hyprpaper = {
       Install.WantedBy = ["hyprland-session.target"];
       Service.Environment = [
-        "XDG_CONFIG_HOME=${hyprpaperConfig}"
+        # "XDG_CONFIG_HOME=${hyprpaperConfig}"
       ];
     };
   };
